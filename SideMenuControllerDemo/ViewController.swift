509be735b40a1714ecc4c4090a83837d97a473ad
Lun Sovathana <lunsovathana@Luns-MacBook-Pro.local>
2016-12-29 23:20:26 +0700

    Initialize

--- SideMenuControllerDemo/ViewController.swift
+++ SideMenuControllerDemo/ViewController.swift
@@ -7,19 +7,18 @@
 //
 
 import UIKit
+import SideMenu
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        SideMenuManager.menuPresentMode = .menuSlideIn
+        SideMenuManager.menuFadeStatusBar = false
+        SideMenuManager.menuEnableSwipeGestures = true
+        SideMenuManager.menuAllowSubmenus = true
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
 
